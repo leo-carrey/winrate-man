d733e50d9c8710195d8947cebc9b0326a3c21c3c
leo-carrey <leo.carrey@laplateforme.io>
2023-11-27 13:40:17 +0100

    [ADD] fonction for get the winrate

--- src/methods/main.py
+++ src/methods/main.py
@@ -1,5 +1,7 @@
+import ast
 import requests
 import json
+import math
 
 api_key = ""
 name = "Maustach"
@@ -8,22 +10,34 @@ start_url_summoner = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/b
 url_summoner = start_url_summoner + name + "?api_key=" + api_key
 
 reponse_summoner = requests.get(url_summoner)
-reponse_summoner.json()
+reponse_summoner_as_json = reponse_summoner.json()
 
-summonerID = reponse_summoner.json()['id']
-accountId = reponse_summoner.json()['accountId']
-puuid = reponse_summoner.json()['puuid']
-summonerLevel = reponse_summoner.json()['summonerLevel']
+summonerID = reponse_summoner_as_json['id']
+accountId = reponse_summoner_as_json['accountId']
+puuid = reponse_summoner_as_json['puuid']
+summonerLevel = reponse_summoner_as_json['summonerLevel']
 
 start_url_match = "https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/"
 end_url_match = "/ids?start=0&count=20&api_key="
 url_match = start_url_match + puuid + end_url_match + api_key
 
 reponse_match = requests.get(url_match)
-reponse_match.json()
+match_text: str = reponse_match.text
+match_text: list = ast.literal_eval(match_text)
 
-print(summonerID)
-print(accountId)
-print(puuid)
-print(summonerLevel)
-print(reponse_match)
+
+def get_winrate():
+    win = 0
+    for i in match_text:
+        match_id = "https://europe.api.riotgames.com/lol/match/v5/matches/" + \
+            i + "?api_key=" + api_key
+        reponse_get_match_by_id = requests.get(match_id)
+        match_by_id_text = reponse_get_match_by_id.text
+        match_by_id_text = json.loads(match_by_id_text)
+        match_win = match_by_id_text["info"]["teams"][1]["win"]
+        if match_win == True:
+            win += 1
+    print((win/20)*100)
+
+
+get_winrate()
